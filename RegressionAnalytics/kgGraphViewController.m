69c84afb1903c2a8ff78a10ecb76cf6b17e69770
Kellen Lask <cisstudent@Maclab04s-MacBook-Pro.local>
2015-05-30 17:36:57 -0600

    Data Class
    Created an outside class to store the data that needs to go between
    tabs. WE CAN STORE ANYTHING!

--- RegressionAnalytics/kgGraphViewController.m
+++ RegressionAnalytics/kgGraphViewController.m
@@ -1,21 +1,22 @@
 //
-//  kgFirstViewController.m
+//  kgGraphViewController.m
 //  RegressionAnalytics
 //
 //  Created by Maclab04 on 5/21/15.
 //  Copyright (c) 2015 __GrahamKellen__. All rights reserved.
 //
 
-#import "kgFirstViewController.h"
+#import "kgGraphViewController.h"
 
 
-@implementation kgFirstViewController
+@implementation kgGraphViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
 	
 } //End viewDidLoad
 
+- (IBAction)zoom:(UIStepper *)sender {
+}
 @end
